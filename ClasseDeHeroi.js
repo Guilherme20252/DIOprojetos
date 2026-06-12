48947219485f18953eaeb32c8b1f26f1b149ef93
GUILHERME HENRIQUE FERREIRA <guilhermeferreira12032007@gmail.com>
2026-06-11 23:16:57 -0300

    Add class selection logic for hero character

---
+++ ClasseDeHeroi.js
@@ -0,0 +1,15 @@
+const classe = gets();
+
+switch(classe) {
+  case "Guerreiro":
+      console.log("Você escolheu a classe Guerreiro!");
+      break;
+   case "Mago":
+   console.log("Você escolheu a classe Mago!");
+    break;
+    case "Arqueiro":
+   console.log("Você escolheu a classe Arqueiro!");
+    break;
+default:
+      console.log("Classe inválida! Escolha entre Guerreiro, Mago ou Arqueiro.");
+}
